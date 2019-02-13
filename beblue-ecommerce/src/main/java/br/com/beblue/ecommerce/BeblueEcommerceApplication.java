afd38b7af7fd3a1cc53b151b66014fd5b508d79e
Raphael Bernoldi <raphaelbernoldi@gmail.com>
2019-02-13 21:00:25 -0200

    Criacao do projeto

---
+++ beblue-ecommerce/src/main/java/br/com/beblue/ecommerce/BeblueEcommerceApplication.java
@@ -0,0 +1,14 @@
+package br.com.beblue.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeblueEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeblueEcommerceApplication.class, args);
+	}
+
+}
+
---
+++ beblue-ecommerce/src/test/java/br/com/beblue/ecommerce/BeblueEcommerceApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.beblue.ecommerce;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BeblueEcommerceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
